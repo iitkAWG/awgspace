0c33115d0d5260fac69b013486711912fb1ad96f
HaoZeke <rohit.goswami@aol.com>
2019-09-07 09:07:42 +0530

    siteJS: Minor update

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -39,7 +39,7 @@ const siteConfig = {
   // For no header links in the top nav bar -> headerLinks: [],
   headerLinks: [
     { doc: "doc1", label: "Caresheets" },
-    { doc: "doc4", label: "Members" },
+    { doc: "doc4", label: "Guidelines" },
     { page: "help", label: "Contact" },
     { blog: true, label: "Milestones" }
   ],
